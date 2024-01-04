d0213a58e20af0d167eaeb4cdd1892021678745a
yzchen14 <yzchen14@gmail.com>
2024-01-05 03:34:42 +0800

    feat: initialize mail archive browser

---
+++ main.py
@@ -0,0 +1,32 @@
+from cefpython3 import cefpython as cef
+import platform
+import sys
+
+
+def main():
+    check_versions()
+
+
+    sys.excepthook = cef.ExceptHook  # To shutdown all CEF processes on error
+    cef.Initialize(settings={}, switches = {"disable-web-security": ""})
+    cef.CreateBrowserSync(url="file:///resource/index.html",
+                          window_title="Mail Archive Browser")
+    cef.MessageLoop()
+    cef.Shutdown()
+
+    
+
+
+def check_versions():
+    ver = cef.GetVersion()
+    print("[hello_world.py] CEF Python {ver}".format(ver=ver["version"]))
+    print("[hello_world.py] Chromium {ver}".format(ver=ver["chrome_version"]))
+    print("[hello_world.py] CEF {ver}".format(ver=ver["cef_version"]))
+    print("[hello_world.py] Python {ver} {arch}".format(
+           ver=platform.python_version(),
+           arch=platform.architecture()[0]))
+    assert cef.__version__ >= "57.0", "CEF Python v57.0+ required to run this"
+
+
+if __name__ == '__main__':
+    main()
