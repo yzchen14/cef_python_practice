204665281b26abce7c0040b69a626d7850f14268
yzchen <yzchen14@gmail.com>
2024-01-07 18:38:57 +0800

    feat: seperate function binding

--- main.py
+++ main.py
@@ -2,28 +2,13 @@ from cefpython3 import cefpython as cef
 import platform
 import sys, ctypes
 
-mytitle = "Test"
-
-
-
 
 class MailArchiveBrowser:
-
-    def check_versions(self):
-        ver = cef.GetVersion()
-        print("[hello_world.py] CEF Python {ver}".format(ver=ver["version"]))
-        print("[hello_world.py] Chromium {ver}".format(ver=ver["chrome_version"]))
-        print("[hello_world.py] CEF {ver}".format(ver=ver["cef_version"]))
-        print("[hello_world.py] Python {ver} {arch}".format(
-            ver=platform.python_version(),
-            arch=platform.architecture()[0]))
-        assert cef.__version__ >= "57.0", "CEF Python v57.0+ required to run this"
-        
-
     def __init__(self):
-        self.check_versions()
         sys.excepthook = cef.ExceptHook
-
+        self.function_binding_dict = {
+            "set_mail_list": self.set_mail_list
+        }
 
     def change_the_window_size_and_icon(self, width, height):
         window_handle = self.browser.GetOuterWindowHandle()
@@ -36,33 +21,20 @@ class MailArchiveBrowser:
         # Set the window icon
         ctypes.windll.user32.SendMessageW(window_handle, 0x80, 0, icon)  # WM_SETICON message code
 
-
     def run(self):
-
         cef.Initialize(settings={}, switches = {"disable-web-security": ""})
-
-
-
         self.browser = cef.CreateBrowserSync(url="file:///resource/index.html",
                           window_title="Mail Archive Browser")
         self.change_the_window_size_and_icon(1200, 600)
-
-
         self.setup_bindings()
-
         cef.MessageLoop()
         cef.Shutdown()
 
     def setup_bindings(self):
         bindings = cef.JavascriptBindings(bindToFrames=False, bindToPopups=False)
-        bindings.SetFunction("printTest", self.test_function)
-        bindings.SetFunction("set_mail_list", self.set_mail_list)
+        for key in self.function_binding_dict:
+            bindings.SetFunction(key, self.function_binding_dict[key])
         self.browser.SetJavascriptBindings(bindings)
-
-
-    def test_function(self, value):
-        print(value)
-
 
     def set_mail_list(self):
         print("Setting Mail List")
